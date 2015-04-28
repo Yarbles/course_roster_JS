111b63cc900d2b18875b570ca258e6fffec857e9
Reid Baldwin & Kyle Giard-Chase <student@epicodus.com>
2015-04-28 11:07:24 -0700

    Add basic directive behaviors to courses.html

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var courseRoster = angular.module('courseRoster', ['ui.router']);
 
+
+// courseRoster.message = "welcome!";
 courseRoster.config(function($stateProvider) {
   $stateProvider.state('home', {
     url: "",
@@ -17,4 +19,37 @@ courseRoster.config(function($stateProvider) {
     templateUrl: "partials/courses.students.html",
     controller: 'StudentsCtrl'
   });
+
+
 });
+
+
+
+courseRoster.directive("welcome", function() {
+    return function (scope, element, attrs) {
+        element.text(scope.app.message + " " + attrs.message);
+    }
+});
+
+courseRoster.directive("enter", function () {
+    return function (scope, element, attrs) {
+        element.bind("mouseenter", function () {
+            element.addClass(attrs.enter);
+        });
+    }
+});
+
+courseRoster.directive("leave", function () {
+    return function (scope, element, attrs) {
+        element.bind("mouseleave", function () {
+            element.removeClass(attrs.enter);
+        });
+    }
+});
+
+// courseRoster.directive("welcometext", function() {
+//   return {
+//      restrict: "E",
+//      template: "<div>Welcome!</div>"
+//   }
+//   });
